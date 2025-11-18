060df918863ec20fa800bb2a46d919e6125c95ae
ibrahim <59414164+ibrahimgb@users.noreply.github.com>
2025-11-18 14:36:06 +0000

    fetch RDS credentials from Secrets Manager

--- ingest_s3_data_to_postgres.py
+++ ingest_s3_data_to_postgres.py
@@ -1,12 +1,16 @@
-from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
+import boto3
+import json
 
-# Initialize Glue and Spark
-sc = SparkContext()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
-job = Job(glueContext)
+def get_secret():
+    secret_name = "phagos-rds-postgresql-credentials"
+    region_name = "eu-north-1"
+    client = boto3.client('secretsmanager', region_name=region_name)
+    try:
+        response = client.get_secret_value(SecretId=secret_name)
+        return json.loads(response['SecretString'])
+    except Exception as e:
+        print(f"Error retrieving secret: {e}")
+        raise
 
-print("Glue job initialized")
-job.commit()
+secret = get_secret()
+print(f"Retrieved RDS credentials for host: {secret['host']}")
