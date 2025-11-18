b9be7bc4e411b6a304662b1de6f06886d70b4f7e
ibrahim <59414164+ibrahimgb@users.noreply.github.com>
2025-11-18 14:54:58 +0000

    table row counts for raw s3 data

---
+++ s3_raw_data_count.py
@@ -0,0 +1,25 @@
+from awsglue.context import GlueContext
+from pyspark.context import SparkContext
+
+# Initialize Glue and Spark
+glueContext = GlueContext(SparkContext.getOrCreate())
+spark = glueContext.spark_session
+
+database = "row-data"
+
+tables = [
+    "dim_raw_genome_metadata",
+    "dim_raw_hosts",
+    "dim_raw_phages",
+    "dim_raw_samples_isolates"
+]
+
+for table in tables:
+    print(f"\nReading table: {table}")
+
+    dyf = glueContext.create_dynamic_frame.from_catalog(database=database, table_name=table)
+    
+    df = dyf.toDF()
+    
+    row_count = df.count()
+    print(f"✔ Table {table} has {row_count} rows")
